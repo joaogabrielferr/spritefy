7c1277f9ea6462adaeca0575ce3df837dc4a9ef1
Joao Gabriel <joaogabrielferr@gmail.com>
2023-05-11 08:49:26 -0300

    fix: fixing paint bucket tool

--- src/Tools/PaintBucket.ts
+++ src/Tools/PaintBucket.ts
@@ -1,8 +1,9 @@
 import Mouse from "../Scene/Mouse";
 import Scene from "../Scene/Scene";
 import { Pixel } from "../types";
+import { BG_COLORS } from "../utils/constants";
 
-export const PaintBucket = (scene : Scene,
+export function PaintBucket(scene : Scene,
     mouse : Mouse,
     pixel_size : number,
     display_size : number,
@@ -10,7 +11,8 @@ export const PaintBucket = (scene : Scene,
       currentScale : number,
       penSize : number,
       CANVAS_SIZE : number,
-      selectedColor : string) => {
+      selectedColor : string){
+
     if (!mouse.isPressed) return [];
   
     const draw : Pixel[] = [];
@@ -19,89 +21,37 @@ export const PaintBucket = (scene : Scene,
   
     if (x > pixel_size * display_size || x < 0 || y > pixel_size * display_size || y < 0) return [];
   
-    let pixel : Pixel | null = null;
-    let flag = false;
-    // let idxi, idxj;
-    for (let i = 0; i < scene.pixels.length; i++) {
-      if (flag) break;
-      for (let j = 0; j < scene.pixels[i].length; j++) {
-        if (x >= scene.pixels[i][j].x1 && x < scene.pixels[i][j].x1 + penSize && y >= scene.pixels[i][j].y1 && y < scene.pixels[i][j].y1 + penSize) {
-          pixel = scene.pixels[i][j];
-        //   idxi = i;
-        //   idxj = j;
-          flag = true;
-          break;
-        }
-      }
-    }
+    let pixel : Pixel | null = scene.findPixel(x,y,pixel_size);
   
     if (pixel != null) {
       //fillSpace(scene.pixels, pixel, selectedColor, pixel.colorStack.top() || pixel.bgColor, pixel_size, display_size, penSize, ctx, draw, CANVAS_SIZE);
       const numPixels = display_size * display_size + 1;
       const visited : boolean[] = [];
       for (let i = 0; i <= numPixels; i++) visited.push(false);
-      bfs(scene.pixels, pixel, visited, selectedColor, pixel.colorStack.top() || pixel.bgColor, penSize, display_size, pixel_size, ctx, draw, CANVAS_SIZE);
+      bfs(scene.pixels, pixel, visited, selectedColor, pixel.colorStack.top() || pixel.bgColor, penSize, ctx, draw, CANVAS_SIZE);
 
     }
   
     return draw;
   }
 
-
-//   function fillSpace(pixels : Pixel[][],
-//      start : Pixel, 
-//      selectedColor : string,
-//       startColor : string, 
-//       pixel_size : number,
-//        display_size : number, 
-//        penSize :number,
-//         ctx : CanvasRenderingContext2D,
-//          draw : Pixel[],
-//           CANVAS_SIZE : number) {
-//     //fill a closed space with the choosen color at once (that paint bucket functionality)
-//     //using BFS
-//     const numPixels = display_size * display_size + 1;
-//     const visited : boolean[] = [];
-//     for (let i = 0; i <= numPixels; i++) visited.push(false);
-//     //dfs(scene.pixels,start,visited,selectedColor,startColor,penSize,display_size,pixel_size,c,draw);
-//     bfs(pixels, start, visited, selectedColor, startColor, penSize, display_size, pixel_size, c, draw, CANVAS_SIZE);
-//     console.log(count);
-//   }
-  
-
-
-function canVisitNeighbor(neighbor : Pixel, visited : boolean[], startColor : string) {
-    return visited[neighbor.id] === false && (neighbor.colorStack.top() === startColor || neighbor.colorStack.isEmpty());
-  }
-  
-  let count = 0;
-  
+  //breadth first search
   function bfs(pixels : Pixel[][], 
     u : Pixel,
      visited : boolean[],
       selectedColor : string,
        startColor : string,
         penSize : number,
-         display_size : number,
-          pixel_size : number,
-           ctx : CanvasRenderingContext2D, 
-           draw : Pixel[],
-            CANVAS_SIZE : number) {
+         ctx : CanvasRenderingContext2D, 
+          draw : Pixel[],
+           CANVAS_SIZE : number) {
 
     visited[u.id] = true;
-    // ctx.fillStyle = selectedColor.value;
-    // ctx.fillRect(u.x1,u.y1,penSize,penSize);
-  
-    // u.color = selectedColor.value;
-    // u.colorStack.push(selectedColor.value);
-    // u.numOfPaints++;
-  
-    // draw.push(u);
+
     const queue : Pixel[] = [];
     queue.push(u);
     while (queue.length > 0) {
       u = queue.shift()!;
-      count++;
       ctx.fillStyle = selectedColor;
       ctx.fillRect(u.x1, u.y1, penSize, penSize);
   
@@ -136,6 +86,9 @@ function canVisitNeighbor(neighbor : Pixel, visited : boolean[], startColor : st
     }
   }
   
+  function canVisitNeighbor(neighbor : Pixel, visited : boolean[], startColor : string) {
+    return visited[neighbor.id] === false && (neighbor.colorStack.top() === startColor || (BG_COLORS.includes(startColor) && neighbor.colorStack.isEmpty()));
+}
 
 
   
