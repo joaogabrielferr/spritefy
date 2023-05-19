c24e0834965690e52f71083b141f7ad904759aa6
Joao Gabriel <joaogabrielferr@gmail.com>
2023-05-19 20:32:30 -0300

    feat: changing icons to tabler-icons

--- src/svg/Line.tsx
+++ src/svg/Line.tsx
@@ -1,5 +1,6 @@
 export function Line(){
-    return <svg fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3.293,20.707a1,1,0,0,1,0-1.414l16-16a1,1,0,1,1,1.414,1.414l-16,16A1,1,0,0,1,3.293,20.707Z"/></svg>
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" shapeRendering="geometricPrecision" textRendering="geometricPrecision"><line x1="10.573759" y1="-11.239978" x2="-10.001086" y2="10.474226" transform="matrix(.998193 0.060088-.060088 0.998193 16.196526 15.750132)" stroke="#000" strokeWidth="3"/></svg>
+
+
 }
 
-//<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
--- src/svg/Square.tsx
+++ src/svg/Square.tsx
@@ -1,7 +1,5 @@
 export function Square(){
-    return <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
-      <path stroke="#000000" strokeLinejoin="round" strokeWidth="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v14a1 1 0 01-1 1H5a1 1 0 01-1-1V5z"/>
-    </svg>
+    return <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" shapeRendering="geometricPrecision" textRendering="geometricPrecision"><line x1="0.028075" y1="-12.353044" x2="-0.028075" y2="12.353044" transform="translate(3.418238 16)" fill="none" stroke="#000104" strokeWidth="3"/><line x1="-0.028075" y1="-11.398784" x2="-0.028075" y2="12.353044" transform="translate(28.602149 16)" fill="none" stroke="#000104" strokeWidth="3"/><line x1="11.541236" y1="-12.353044" x2="-16.567226" y2="-12.353044" transform="translate(18.512995 16.95426)" fill="none" stroke="#000104" strokeWidth="3"/><line x1="11.541236" y1="-12.353044" x2="-16.567226" y2="-12.353044" transform="translate(18.512995 39.207662)" fill="none" stroke="#000104" strokeWidth="3"/></svg>
+
 }
 
-// <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
