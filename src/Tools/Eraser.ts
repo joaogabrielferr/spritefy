2c346b0d6ff07d75bbf225fb3140d676a3e1e229
Joao Gabriel <joaogabrielferr@gmail.com>
2023-05-26 14:34:47 -0300

    fix: fixing eraser

--- src/Tools/Eraser.ts
+++ src/Tools/Eraser.ts
@@ -28,44 +28,52 @@ export function Eraser(eventName : string, mouse : Mouse, scene : Scene, pixel_s
     //if pixel is empty (not painted) dont do anything
     if (pixel != null) {
 
+        console.log(!isPixelAlreadyPaintedInCurrentDraw(pixel, scene) && !empty(pixel));
           if(!isPixelAlreadyPaintedInCurrentDraw(pixel, scene) && !empty(pixel))
           {
               pixel.colorStack.push(pixel.bgColor);
               scene.currentPixelsMousePressed.set(pixel.id, true);
               draw.push(pixel);
-
+              ctx.fillStyle = pixel.bgColor;
+              ctx.fillRect(pixel.x1, pixel.y1, pixel_size, pixel_size);
           }
-          ctx.fillStyle = pixel.bgColor;
-          ctx.fillRect(pixel.x1, pixel.y1, pixel_size, pixel_size);
-
+        //   if(!empty(pixel))
+        //   {
+        //   }
 
           if(penSize === 2)
             {
                 const neighbors = scene.findNeighborsSize2(pixel);
                 for(let n of neighbors)
                 {
-                    if(!isPixelAlreadyPaintedInCurrentDraw(n, scene))
+                    if(!isPixelAlreadyPaintedInCurrentDraw(n, scene) && !empty(n))
                     {
                         scene.currentPixelsMousePressed.set(n.id, true);
                         n.colorStack.push(n.bgColor);
                         draw.push(n);
-                    }
                         ctx.fillStyle = n.bgColor;
                         ctx.fillRect(n.x1, n.y1, pixel_size, pixel_size);
+                    }
+                    // if(!empty(n))
+                    // {
+                    // }
                 }
             }else if(penSize === 3)
             {
                 const neighbors = scene.findNeighborsSize3(pixel);
                 for(let n of neighbors)
                 {
-                    if(!isPixelAlreadyPaintedInCurrentDraw(n, scene))
+                    if(!isPixelAlreadyPaintedInCurrentDraw(n, scene) && !empty(n))
                     {
                         scene.currentPixelsMousePressed.set(n.id, true);
                         n.colorStack.push(n.bgColor);
                         draw.push(n);
-                    }
                         ctx.fillStyle = n.bgColor;
                         ctx.fillRect(n.x1, n.y1, pixel_size, pixel_size);
+                    }
+                    // if(!empty(n))
+                    // {
+                    // }
                 }
             }
 
@@ -76,41 +84,49 @@ export function Eraser(eventName : string, mouse : Mouse, scene : Scene, pixel_s
           //build path from last pixel to current pixel
           const path = bresenhamsAlgorithm(scene, scene.lastPixel, pixel,pixel_size);
           for (let p of path) {
-              if (!isPixelAlreadyPaintedInCurrentDraw(p, scene)) {
+              if (!isPixelAlreadyPaintedInCurrentDraw(p, scene) && !empty(p)) {
                   p.colorStack.push(p.bgColor);
                   scene.currentPixelsMousePressed.set(p.id, true);
                   draw.push(p);
+                  ctx.fillStyle = p.bgColor;
+                  ctx.fillRect(p.x1, p.y1, pixel_size, pixel_size);
               }
-              ctx.fillStyle = p.bgColor;
-              ctx.fillRect(p.x1, p.y1, pixel_size, pixel_size);
-              
+            //   if(!empty(p))
+            //   {
+            //   }
               if(penSize === 2)
                 {
                     const neighbors = scene.findNeighborsSize2(p);
                     for(let n of neighbors)
                     {
-                        if(!isPixelAlreadyPaintedInCurrentDraw(n, scene))
+                        if(!isPixelAlreadyPaintedInCurrentDraw(n, scene) && !empty(n))
                         {
                             scene.currentPixelsMousePressed.set(n.id, true);
                             n.colorStack.push(n.bgColor);
                             draw.push(n);
-                        }
                             ctx.fillStyle = n.bgColor;
                             ctx.fillRect(n.x1, n.y1, pixel_size, pixel_size);
+                        }
+                        // if(!empty(n))
+                        // {
+                        // }
                     }
                 }else if(penSize === 3)
                 {
                     const neighbors = scene.findNeighborsSize3(p);
                     for(let n of neighbors)
                     {
-                        if(!isPixelAlreadyPaintedInCurrentDraw(n, scene))
+                        if(!isPixelAlreadyPaintedInCurrentDraw(n, scene) && !empty(n))
                         {
                             scene.currentPixelsMousePressed.set(n.id, true);
                             n.colorStack.push(n.bgColor);
                             draw.push(n);
-                        }
                             ctx.fillStyle = n.bgColor;
                             ctx.fillRect(n.x1, n.y1, pixel_size, pixel_size);
+                        }
+                        // if(!empty(n))
+                        // {
+                        // }
                     }
                 }
 
