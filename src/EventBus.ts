5a46362de0b49a7015c00345430ada30f6aa5974
Joao Gabriel <joaogabrielferr@gmail.com>
2023-05-25 21:28:06 -0300

    feat: add eventbus for communication between unralated components

---
+++ src/EventBus.ts
@@ -0,0 +1,42 @@
+import { IEventBus, Subscription, Subscriber } from "./types";
+
+//for triggering events from unrelated components
+export class EventBus implements IEventBus {
+
+    
+    private subscribers: Subscriber;
+    private static nextId = 0;
+  
+    constructor() {
+      this.subscribers = {};
+    }
+  
+    public publish<T>(event: string, arg?: T): void {
+      const subscriber = this.subscribers[event];
+  
+      if (!subscriber) {
+        return;
+      }
+  
+      Object.keys(subscriber).forEach((id) => subscriber[id](arg));
+    }
+  
+    public subscribe(event: string, callback: ()=> void): Subscription {
+      const id = this.getNextId();
+      if (!this.subscribers[event]) this.subscribers[event] = {};
+  
+      this.subscribers[event][id] = callback;
+  
+      return {
+        unsubscribe: () => {
+          delete this.subscribers[event][id];
+          if (Object.keys(this.subscribers[event]).length === 0)
+            delete this.subscribers[event];
+        },
+      };
+    }
+  
+    private getNextId(): number {
+      return EventBus.nextId++;
+    }
+  }
--- src/types/index.ts
+++ src/types/index.ts
@@ -18,3 +18,20 @@ export type toolsType = 'pencil' | 'eraser' | 'paintBucket' | 'dropper' | 'line'
 export type ToolButton= {tool : toolsType, svg : JSX.Element,tooltip:string};
 
 export type PaletteType = {name : string,colors: string[],id : number};
+
+export type Subscription = {
+    unsubscribe: () => void;
+}
+  
+export type Callback = {
+[key: string]: <T>(a : T)=> void;
+}
+
+export type Subscriber = {
+[key: string]: Callback;
+}
+
+export type IEventBus = {
+publish<T>(event: string, arg?: T): void;
+subscribe(event: string, callback: ()=> void) : Subscription;
+}
