aa2054c4942c24f08f797e6a0e617206a3611fa9
Joao Gabriel <joaogabrielferr@gmail.com>
2023-08-10 18:56:48 -0300

    feat: add mobile menu for topbar

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './topbar.scss';
-import { faArrowRotateLeft, faBars, faBroom, faDownload, faFile, faRotateRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBroom, faDownload, faFile } from '@fortawesome/free-solid-svg-icons';
 import { EventBus } from '../../EventBus';
-import { CLEAR_DRAWING, REDO_LAST_DRAW, START_NEW_DRAWING, UNDO_LAST_DRAW } from '../../utils/constants';
+import { CLEAR_DRAWING, START_NEW_DRAWING } from '../../utils/constants';
 import { store, StoreType } from '../../store';
 import { createSpriteSheet } from '../../algorithms/createSpriteSheet';
+import { useState } from 'react';
 
 export function Topbar({ isMobile }: { isMobile: boolean }) {
   const displaySize = store((store: StoreType) => store.displaySize);
@@ -12,6 +13,8 @@ export function Topbar({ isMobile }: { isMobile: boolean }) {
   const framesList = store((store: StoreType) => store.framesList);
 
   const frameRate = store((store: StoreType) => store.frameRate);
+
+  const [isTopBarMobileOpen, setIsTopBarMobileOpen] = useState(false);
 
   function downloadDrawing() {
     createSpriteSheet(displaySize, framesList);
@@ -93,28 +96,45 @@ export function Topbar({ isMobile }: { isMobile: boolean }) {
   //     });
 
   return (
-    <div className="topbar">
-      <div className="inner-topbar">
-        {!isMobile ? (
-          <nav className="topbar-nav">
-            <button onClick={() => EventBus.getInstance().publish(START_NEW_DRAWING)}>
-              <FontAwesomeIcon size="lg" color="white" icon={faFile} /> NEW DRAWING
-            </button>
-            <button onClick={() => EventBus.getInstance().publish(CLEAR_DRAWING)}>
-              <FontAwesomeIcon size="lg" color="white" icon={faBroom} />
-              CLEAR DRAWING
-            </button>
-            <button className="topbar-download" onClick={downloadDrawing}>
-              <FontAwesomeIcon size="lg" color="#abbbc7" icon={faDownload} />
-              SAVE DRAWING
-            </button>
-          </nav>
-        ) : (
-          <div>
-            <FontAwesomeIcon size="2x" color="white" icon={faBars} />
-          </div>
-        )}
+    <>
+      <div className="topbar">
+        <div className="inner-topbar">
+          {!isMobile ? (
+            <nav className="topbar-nav">
+              <button onClick={() => EventBus.getInstance().publish(START_NEW_DRAWING)}>
+                <FontAwesomeIcon size="lg" color="white" icon={faFile} /> NEW DRAWING
+              </button>
+              <button onClick={() => EventBus.getInstance().publish(CLEAR_DRAWING)}>
+                <FontAwesomeIcon size="lg" color="white" icon={faBroom} />
+                CLEAR DRAWING
+              </button>
+              <button className="topbar-download" onClick={downloadDrawing}>
+                <FontAwesomeIcon size="lg" color="#abbbc7" icon={faDownload} />
+                SAVE DRAWING
+              </button>
+            </nav>
+          ) : (
+            <div onClick={() => setIsTopBarMobileOpen((prev) => !prev)}>
+              <FontAwesomeIcon size="2x" color="white" icon={faBars} />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className={`topbar-mobile ${isTopBarMobileOpen ? 'open' : ''}`}>
+        <nav className="topbar-nav">
+          <button onClick={() => EventBus.getInstance().publish(START_NEW_DRAWING)}>
+            <FontAwesomeIcon size="lg" color="white" icon={faFile} /> NEW DRAWING
+          </button>
+          <button onClick={() => EventBus.getInstance().publish(CLEAR_DRAWING)}>
+            <FontAwesomeIcon size="lg" color="white" icon={faBroom} />
+            CLEAR DRAWING
+          </button>
+          <button className="topbar-download" onClick={downloadDrawing}>
+            <FontAwesomeIcon size="lg" color="#abbbc7" icon={faDownload} />
+            SAVE DRAWING
+          </button>
+        </nav>
+      </div>
+    </>
   );
 }
