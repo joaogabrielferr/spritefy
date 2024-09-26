344acb1dea1320a2f5c39a994e1f5243f544f58c
Joao Gabriel <joaogabrielferr@gmail.com>
2024-09-25 22:23:29 -0300

    fix: fix import

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from 'react';
-import './sideBar.scss';
+import './sidebar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
