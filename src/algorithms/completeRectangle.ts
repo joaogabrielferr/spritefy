7bb48e4cb605a8966278a97a612fbee860c9258f
Joao Gabriel <joaogabrielferr@gmail.com>
2024-09-25 22:20:23 -0300

    fix: fix variable in rectangle tool

--- src/algorithms/completeRectangle.ts
+++ src/algorithms/completeRectangle.ts
@@ -9,36 +9,36 @@ export function completeRectangle(
 
   if (start.x <= end.x) {
     for (let i = start.x; i <= end.x; i += pixel_size) {
-      findAndPush(i, start.y, pixel_size, path, display_size);
-      findAndPush(i, end.y, pixel_size, path, display_size);
+      findAndPush(i, start.y, path, display_size);
+      findAndPush(i, end.y, path, display_size);
     }
   }
 
   if (start.x > end.x) {
     for (let i = end.x; i <= start.x; i += pixel_size) {
-      findAndPush(i, start.y, pixel_size, path, display_size);
-      findAndPush(i, end.y, pixel_size, path, display_size);
+      findAndPush(i, start.y, path, display_size);
+      findAndPush(i, end.y, path, display_size);
     }
   }
 
   if (start.y <= end.y) {
     for (let i = start.y; i <= end.y; i += pixel_size) {
-      findAndPush(start.x, i, pixel_size, path, display_size);
-      findAndPush(end.x, i, pixel_size, path, display_size);
+      findAndPush(start.x, i, path, display_size);
+      findAndPush(end.x, i, path, display_size);
     }
   }
 
   if (start.y > end.y) {
     for (let i = end.y; i <= start.y; i += pixel_size) {
-      findAndPush(start.x, i, pixel_size, path, display_size);
-      findAndPush(end.x, i, pixel_size, path, display_size);
+      findAndPush(start.x, i, path, display_size);
+      findAndPush(end.x, i, path, display_size);
     }
   }
 
   return path;
 }
 
-function findAndPush(a: number, b: number, pixel_size: number, path: { x: number; y: number }[], display_size: number) {
+function findAndPush(a: number, b: number, path: { x: number; y: number }[], display_size: number) {
   if (a >= 0 && a < display_size && b >= 0 && b < display_size) {
     path.push({ x: Math.floor(a), y: Math.floor(b) });
   }
