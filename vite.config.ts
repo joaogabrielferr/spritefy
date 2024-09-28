5bfbfb632fa7ffd17959a4b4964d7d79ca7e5b23
Joao Gabriel <joaogabrielferr@gmail.com>
2024-09-27 21:09:47 -0300

    fix base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()]
+  plugins: [svgr(), react()],
+  base: '/spritefy'
 });
